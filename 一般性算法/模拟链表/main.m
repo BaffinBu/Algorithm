7336e0b4249c13ba2f27e418cbf39bd13ac4a58f
Mr.Bu <530275768@qq.com>
2016-07-28 20:29:17 +0800

    链表的简单实现

---
+++ 一般性算法/模拟链表/main.m
@@ -0,0 +1,66 @@
+//
+//  main.m
+//  模拟链表
+//
+//  Created by wy0012 on 16/7/27.
+//  Copyright © 2016年 wy0012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//typedef struct node{
+//    void * data;
+//    struct node * next;
+//} linkNode;
+//
+//typedef struct{
+//    
+//    linkNode * head;
+//    linkNode * tail;
+//    int length;
+//    
+//}LinkList;
+//
+//LinkList getLinkedList(){
+//    LinkList list;
+//    linkNode node;
+//    node.data=0;
+//    node.next=NULL;
+//    list.head=&node;
+//    list.tail=&node;
+//    return list;
+//}
+//
+//
+//void insertFromHead(int num){
+//    
+//}
+
+
+typedef struct{
+    int num;
+    struct Node * next;
+}Node;
+
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+        for(int i=0;i<10;i++){
+            Node node;
+            node.num=i;
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+    return 0;
+}
